1455f01acbce8446c217b59f3f4c8402900f2d58
Titus Brown <titus@idyll.org>
2018-01-18 08:54:26 -0500

    updates for linux

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
+EXTRA_COMPILE_ARGS=[]
+if sys.platform == 'darwin':              # Mac OS X?
+    EXTRA_COMPILE_ARGS.extend(['-arch', 'x86_64', '-mmacosx-version-min=10.7',
+                               '-std=c++11', '-stdlib=libc++'])
+ 
 
 setup(
    name='bbhash',
@@ -13,6 +20,6 @@ setup(
    ext_modules =
           [Extension('bbhash', sources=['bbhash.pyx', 'bbhash-wrap.cc'],
                      language='c++',
-                     extra_compile_args=['-std=c++11', '-stdlib=libc++'])],
+                     extra_compile_args=EXTRA_COMPILE_ARGS)],
    cmdclass = {'build_ext': build_ext}
 )
