c4c095081caf906e807a581100ce0392247bbae9
C. Titus Brown <titus@idyll.org>
2018-01-25 06:32:57 -0800

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.platform == 'darwin':              # Mac OS X?
 
 setup(
    name='bbhash',
-   version='0.1dev',
+   version='0.1dev2',
    description="A Python wrapper for the BBHash Minimal Perfect Hash Function",
    author="C. Titus Brown",
    author_email="titus@idyll.org",
