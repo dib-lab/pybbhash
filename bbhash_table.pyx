e5c66c23609afac86794780a21605c3770eec6e9
C. Titus Brown <titus@idyll.org>
2020-10-22 06:38:01 -0700

    kmer -> hash in table code

--- bbhash_table.pyx
+++ bbhash_table.pyx
@@ -3,33 +3,40 @@ import bbhash
 import os
 
 class BBHashTable(object):
+    """\
+    Retrieve values by MPHF lookup.
+
+    Handles the situation where the query hash may not be in the table.
+    """
     def __init_(self):
         pass
 
-    def initialize(self, all_kmers, fill=0, dtype=numpy.uint32):
-        self.mphf_to_kmer = numpy.zeros(len(all_kmers), numpy.uint64)
-        self.mphf_to_value = numpy.full(len(all_kmers), fill, dtype)
-        self.mphf = bbhash.PyMPHF(all_kmers, len(all_kmers), 4, 1.0,)
+    def initialize(self, all_hashes, fill=0, dtype=numpy.uint32):
+        # MPHF -> hash
+        self.mphf_to_hash = numpy.zeros(len(all_hashes), numpy.uint64)
+        # MPHF -> stored value 
+        self.mphf_to_value = numpy.full(len(all_hashes), fill, dtype)
+        self.mphf = bbhash.PyMPHF(all_hashes, len(all_hashes), 4, 1.0,)
 
-        for k in all_kmers:
+        for k in all_hashes:
             mp_hash = self.mphf.lookup(k)
-            self.mphf_to_kmer[mp_hash] = k
+            self.mphf_to_hash[mp_hash] = k
 
     def __len__(self):
-        return len(self.mphf_to_kmer)
+        return len(self.mphf_to_hash)
 
-    def __getitem__(self, kmer_hash, default_val=None):
-        mp_hash = self.mphf.lookup(kmer_hash)
+    def __getitem__(self, hashval, default_val=None):
+        mp_hash = self.mphf.lookup(hashval)
         if mp_hash is None:
             return default_val
-        elif self.mphf_to_kmer[mp_hash] == kmer_hash:   # found!
+        elif self.mphf_to_hash[mp_hash] == hashval:   # found!
             return self.mphf_to_value[mp_hash]
         return default_val
 
-    def __setitem__(self, kmer_hash, value):
-        mp_hash = self.mphf.lookup(kmer_hash)
-        if mp_hash is None or self.mphf_to_kmer[mp_hash] != kmer_hash:
-            raise ValueError("given kmer_hash is unknown to mphf")
+    def __setitem__(self, hashval, value):
+        mp_hash = self.mphf.lookup(hashval)
+        if mp_hash is None or self.mphf_to_hash[mp_hash] != hashval:
+            raise ValueError("given hashval is unknown to mphf")
 
         self.mphf_to_value[mp_hash] = value
 
@@ -43,7 +50,7 @@ class BBHashTable(object):
         self.mphf.save(mphf_filename)
         with open(array_filename, "wb") as fp:
             numpy.savez_compressed(fp,
-                                   mphf_to_kmer=self.mphf_to_kmer,
+                                   mphf_to_hash=self.mphf_to_hash,
                                    mphf_to_value=self.mphf_to_value)
 
     @classmethod
@@ -53,12 +60,12 @@ class BBHashTable(object):
         mphf = bbhash.load_mphf(mphf_filename)
         with open(array_filename, "rb") as fp:
             np_dict = numpy.load(fp)
-            mphf_to_kmer = np_dict["mphf_to_kmer"]
+            mphf_to_hash = np_dict["mphf_to_hash"]
             mphf_to_value = np_dict["mphf_to_value"]
 
         obj = cls()
         obj.mphf = mphf
-        obj.mphf_to_kmer = mphf_to_kmer
+        obj.mphf_to_hash = mphf_to_hash
         obj.mphf_to_value = mphf_to_value
 
         return obj
